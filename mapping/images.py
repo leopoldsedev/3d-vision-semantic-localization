1a297a6b8f3ebfc076e2dfc901d835c4c85f49f0
Christian Leopoldseder <cleopolds@student.ethz.ch>
2020-04-23 22:17:57 +0200

    Sort image paths when reading from image directory.

--- mapping/images.py
+++ mapping/images.py
@@ -3,7 +3,7 @@ from os.path import join, basename
 
 
 def get_image_path_list(image_dir_path):
-    return glob.glob(join(image_dir_path, '*.jpg'))#[50:-50] # TODO Undo
+    return sorted(glob.glob(join(image_dir_path, '*.jpg')))#[50:-50] # TODO Undo
 
 def get_image_names(image_dir_path):
     image_paths = get_image_path_list(image_dir_path)
