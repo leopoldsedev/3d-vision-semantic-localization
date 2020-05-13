15f8f7141815c3c727ba5b30c4cd7bc6231bd56d
Pedro Roig Aparicio <peroap@gmail.com>
2020-05-13 19:12:25 +0200

    transform coordinates of overlapping routes to local7

--- mapping/calculate-local-transformation.py
+++ mapping/calculate-local-transformation.py
@@ -8,8 +8,15 @@ from scipy import integrate
 
 np.set_printoptions(threshold=100, linewidth=200)
 
+#Get Matrix H_GL
+print("-"*20)
+print()
+print()
+print("Get Matrix H_GL and H_LG:")
+print()
+print()
 
-gps_full_data = np.genfromtxt('07/gps.csv', skip_header=1)
+gps_full_data = np.genfromtxt('MalagaDataSet_Routes/malaga-urban-dataset-extract-15_all-sensors_GPS.txt', skip_header=1)
 
 gps_timestamps = gps_full_data[:,0]
 gps_geocen_x = gps_full_data[:,12]
@@ -36,12 +43,12 @@ for i in range(1, len(gps_local_x)):
 t = np.array(gps_geocen[0])
 
 # Choose 3 linearly independent points
-g1 = gps_geocen[1]
-g2 = gps_geocen[2]
-g3 = gps_geocen[3]
-l1 = gps_local[1]
-l2 = gps_local[2]
-l3 = gps_local[3]
+route_g1 = gps_geocen[1]
+route_g2 = gps_geocen[2]
+route_g3 = gps_geocen[3]
+route_l1 = gps_local[1]
+route_l2 = gps_local[2]
+route_l3 = gps_local[3]
 
 if np.linalg.matrix_rank(gps_geocen[1:4]) != 3 and np.linalg.matrix_rank(gps_local[1:4]) != 3:
     print("WARNING: Chosen vectors are not linearly independent")
@@ -49,26 +56,26 @@ if np.linalg.matrix_rank(gps_geocen[1:4]) != 3 and np.linalg.matrix_rank(gps_loc
 # Build linear system of equations that constrains the rotation matrix
 # TODO This could be done with all vectors in the list and the pseudo inverse would find the optimal fit
 A = np.array([
-    [l1[0], l1[1], l1[2], 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, l1[0], l1[1], l1[2], 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, l1[0], l1[1], l1[2]],
-    [l2[0], l2[1], l2[2], 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, l2[0], l2[1], l2[2], 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, l2[0], l2[1], l2[2]],
-    [l3[0], l3[1], l3[2], 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, l3[0], l3[1], l3[2], 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, l3[0], l3[1], l3[2]],
+    [route_l1[0], route_l1[1], route_l1[2], 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, route_l1[0], route_l1[1], route_l1[2], 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, route_l1[0], route_l1[1], route_l1[2]],
+    [route_l2[0], route_l2[1], route_l2[2], 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, route_l2[0], route_l2[1], route_l2[2], 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, route_l2[0], route_l2[1], route_l2[2]],
+    [route_l3[0], route_l3[1], route_l3[2], 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, route_l3[0], route_l3[1], route_l3[2], 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, route_l3[0], route_l3[1], route_l3[2]],
     ])
 b = np.array([
-    g1[0] - t[0],
-    g1[1] - t[1],
-    g1[2] - t[2],
-    g2[0] - t[0],
-    g2[1] - t[1],
-    g2[2] - t[2],
-    g3[0] - t[0],
-    g3[1] - t[1],
-    g3[2] - t[2],
+    route_g1[0] - t[0],
+    route_g1[1] - t[1],
+    route_g1[2] - t[2],
+    route_g2[0] - t[0],
+    route_g2[1] - t[1],
+    route_g2[2] - t[2],
+    route_g3[0] - t[0],
+    route_g3[1] - t[1],
+    route_g3[2] - t[2],
     ])
 
 # Solve the system
@@ -86,14 +93,36 @@ H_GL = tf.affines.compose(t, R, np.ones(3), np.zeros(3))
 # Calculate the inverse
 H_LG = tf.affines.compose(np.dot(-R.T, t), R.T, np.ones(3), np.zeros(3))
 
+print("Full 'local to geocen'-matrix\n")
 print(H_GL)
+print()
+print("Full 'geocen to local'-matrix\n")
 print(H_LG)
+print()
+
+#Print dot product to prove that the inverse was done correctly
+print("Sanity check:")
 print(np.dot(H_LG, H_GL))
+print()
 
 # Check result
-for i in range(0, len(gps_local_x)):
+for i in range(0, len(gps_local_y)):
     local_homogen = np.block([gps_local[i], 1])[np.newaxis].T
     geocen_transformed = np.dot(H_GL, local_homogen)[:-1]
     dist_geocen = np.linalg.norm(geocen_transformed - gps_geocen[i][np.newaxis].T)
     if not np.isclose(dist_geocen, 0):
         print("There is point that is not transformed right")
+
+np.save("transform_matrices/H15_GL",H_GL)
+
+
+#Check import
+print("-"*20)
+print()
+print()
+print("Check import")
+print()
+print()
+
+H_LG_imported = np.load('transform_matrices/HX_LG.npy') #adapt
+print(H_LG_imported)
