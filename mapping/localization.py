921ae77162ef618690476552589cfc40ba1d6d5e
Christian Leopoldseder <cleopolds@student.ethz.ch>
2020-05-14 17:41:20 +0200

    Rework score calculation, implement localization heatmap.

--- mapping/localization.py
+++ mapping/localization.py
@@ -2,88 +2,191 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 import transforms3d as tf
+from numpy import interp
 
 import prediction
 import score
 import detection
 import mapping
 import util
-from triangulation import MapLandmark, ImagePose, get_camera_malaga_extract_07_right, ColmapCamera
+from triangulation import MapLandmark, ImagePose, get_camera_malaga_extract_07_right, ColmapCamera, malaga_car_pose_to_camera_pose
 
 MAP_PATH = './map.pickle'
 QUERY_IMAGE_PATH = './07/images/rectified/img_CAMERA1_1261230001.030210_right.jpg'
+#QUERY_IMAGE_PATH = './07/images/rectified/img_CAMERA1_1261229994.680132_right.jpg'
 
-def get_gaussian(sigma, mu):
-    x, y = np.meshgrid(np.linspace(-1,1,10), np.linspace(-1,1,10))
-    d = np.sqrt(x*x+y*y)
-    g = np.exp(-( (d-mu)**2 / ( 2.0 * sigma**2 ) ) )
-    return g
+POSITION_STEP_SIZE = 5
+ANGLE_STEP_SIZE = 10
+LANDMARK_MARGIN = prediction.LANDMARK_RELEVANCE_RANGE
 
+
+def get_possible_poses(landmark_list, position_step_size, angle_step_size, landmark_margin):
+    num_angles = 360 / angle_step_size
+    # Check if angle step size divides into whole numbers
+    assert(np.allclose(num_angles, np.floor(num_angles)))
+
+    landmark_x_low = np.min([landmark.x for landmark in landmark_list])
+    landmark_y_low = np.min([landmark.y for landmark in landmark_list])
+    landmark_x_high = np.max([landmark.x for landmark in landmark_list])
+    landmark_y_high = np.max([landmark.y for landmark in landmark_list])
+
+    x_low = landmark_x_low - landmark_margin
+    y_low = landmark_y_low - landmark_margin
+    x_high = landmark_x_high + landmark_margin
+    y_high = landmark_y_high + landmark_margin
+
+    x_steps = range(int(np.floor(x_low)), int(np.ceil(x_high)) + position_step_size, position_step_size)
+    y_steps = range(int(np.floor(y_low)), int(np.ceil(y_high)) + position_step_size, position_step_size)
+    angle_steps = range(0, 360, angle_step_size)
+
+    possible_poses = np.zeros((len(x_steps), len(y_steps), len(angle_steps), 7))
+
+    for i, x in enumerate(x_steps):
+        for j, y in enumerate(y_steps):
+            for k, yaw_deg in enumerate(angle_steps):
+                position_car = np.array([x, y, 0.0])
+                orientation_car = tf.euler.euler2quat(np.deg2rad(-90), 0, np.deg2rad(yaw_deg), 'sxyz')
+
+                position_camera, orientation_camera = malaga_car_pose_to_camera_pose(position_car, orientation_car, right=True)
+
+                possible_poses[i, j, k] = np.hstack((position_camera, orientation_camera))
+
+    return possible_poses
+
+
+score_calc_count = 0
 def get_pose_scores(landmark_list, query_image, camera, sign_types):
+    # Filter only landmarks of given sign types
+    landmark_list = list(filter(lambda l: l.sign_type in sign_types, landmark_list))
+
     query_detections, debug_image = detection.detect_traffic_signs_in_image(query_image, sign_types)
-    #plt.imshow(util.bgr_to_rgb(debug_image))
-    #plt.show()
+    # TODO Uncomment
+#     detection1 = detection.TrafficSignDetection(x=809.0, y=408.0, width=38, height=38, sign_type=detection.TrafficSignType.CROSSING, score=0.8859539)
+#     detection2 = detection.TrafficSignDetection(x=205.0, y=428.0, width=30, height=30, sign_type=detection.TrafficSignType.CROSSING, score=0.89329803)
+#     query_detections = [detection1, detection2]
+    plt.imshow(util.bgr_to_rgb(debug_image))
+    plt.show(block=False)
+    # Pause so that the window gets drawn
+    plt.pause(0.0001)
 
-    # TODO Use actual function
-    #possible_camera_poses = get_possible_poses(landmark_list)
-    size = 50
-    possible_camera_poses = np.random.random((size, size, 8, 7))
-    possible_camera_poses[:,:,:,2] = 0.0
-    for x in range(possible_camera_poses.shape[0]):
-        for y in range(possible_camera_poses.shape[1]):
-            scale = 1
-            possible_camera_poses[x,y,:,0] = x * scale
-            possible_camera_poses[x,y,:,1] = y * scale
+    possible_camera_poses = get_possible_poses(landmark_list, POSITION_STEP_SIZE, ANGLE_STEP_SIZE, LANDMARK_MARGIN)
 
-            for i in range(possible_camera_poses.shape[2]):
-                yaw = np.random.random() * 360
-                orientation = np.array(tf.euler.euler2quat(np.deg2rad(yaw), 0, 0, axes='szyx'))
-                possible_camera_poses[x,y,i,3:7] = orientation
+    empty_predicted_score = score.get_score([], query_detections, sign_types, debug=False)
+
+    total_poses = possible_camera_poses.size / 7
+    score_calc_count = 0
 
     def calculate_score(pose):
-        # TODO Calculate and return actual score
-        #predicted_detections = prediction.predicted_detections(pose, landmark_list, camera)
-        #score = get_score(query_detections, predicted_detections, sign_types)
-        #return score
-        orientation = pose[3:7]
-        yaw, _, _ = tf.euler.quat2euler(orientation, axes='szyx')
-        if yaw < 0:
-            yaw += 2 * np.pi
-        return yaw
+        position, orientation = split_pose_array(pose)
+        image_pose = ImagePose(position=position, orientation=orientation)
+
+        debug = False
+        dist = np.linalg.norm(np.array([150.0, -6.0]) - position[0:2])
+        #if dist < 2:
+            #debug = True
+
+        predicted_detections = prediction.predicted_detections(image_pose, landmark_list, camera, debug=debug)
+
+        _, _, yaw = tf.euler.quat2euler(orientation, axes='sxyz')
+
+        if debug:
+            debug_image = np.zeros((camera.height, camera.width, 3))
+            debug_image = detection.generate_debug_image(debug_image, predicted_detections)
+            plt.imshow(util.bgr_to_rgb(debug_image)/255)
+            plt.show()
+
+        # If there are no detections predicted use the cached score for that case
+        if len(predicted_detections) == 0:
+            score_val = empty_predicted_score
+        else:
+            score_val = score.get_score(predicted_detections, query_detections, sign_types, debug=debug)
+
+        global score_calc_count
+        score_calc_count += 1
+        progress = 100 * score_calc_count / total_poses
+        print('{:.2f}%    '.format(progress), end='\r')
+        return score_val
 
     scores = np.apply_along_axis(calculate_score, 3, possible_camera_poses)
-    print(scores.shape)
-    print(np.mean(np.max(scores, axis=2)))
+
+    print('Done     ')
 
     return possible_camera_poses, scores
 
 
+def split_pose_array(pose_arr):
+    assert(pose_arr.shape == (7,))
+    position = pose_arr[0:3]
+    orientation = pose_arr[3:7]
+    return position, orientation
+
+
 def show_heatmap(possible_poses, pose_scores, landmark_list):
-    print(pose_scores.shape)
     highest_score_per_position = np.max(pose_scores, axis=2)
     highest_score_pose_idx = np.argmax(pose_scores, axis=2)
-    print(highest_score_per_position.shape)
-    print(highest_score_pose_idx.shape)
+
+    extent = [
+        np.min(possible_poses[:,0,0,0]) - POSITION_STEP_SIZE/2,
+        np.max(possible_poses[:,0,0,0]) + POSITION_STEP_SIZE/2,
+        # The y-axis is flipped intentionally (because in image coordinates the y-axis starts from the top-left corner)
+        np.max(possible_poses[0,:,0,1]) + POSITION_STEP_SIZE/2,
+        np.min(possible_poses[0,:,0,1]) - POSITION_STEP_SIZE/2,
+    ]
+
+    # Adjust scores to look nicer in visualization
+    # TODO Maybe don't do this if query image has no detections
+    empty_predicted_score = score.get_score([], [], sign_types, debug=False)
+    highest_scores_adjusted = interp(highest_score_per_position, [empty_predicted_score, 1], [0, 1])
+    heatmap_values = highest_scores_adjusted.T
 
     # Visualize as a heatmap
-    #plt.imshow(highest_score_per_position, cmap='hot', interpolation='nearest')#, extent=[0, 200, -100, 100])
-    plt.imshow(highest_score_per_position, cmap='viridis')#, extent=[0, 50, -100, 100])
-    plt.colorbar()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    #ax.imshow(heatmap_values.T, cmap='hot', interpolation='nearest', extent=[0, 200, -100, 100])
+    #ax.imshow(heatmap_values.T, cmap='viridis', extent=extent)
+    cax = fig.add_axes([0.92, 0.11, 0.02, 0.77])
+    im = ax.imshow(heatmap_values, cmap='Greens', extent=extent)
+    #im.set_clim(0.0, 1.0)
+    fig.colorbar(im, cax=cax)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.axis('equal')
+    ax.set_xlim((extent[0], extent[1]))
+    # Note that the y limits were flipped in extent
+    ax.set_ylim((extent[3], extent[2]))
 
-    # Visualize orientations with highest score
+    poses = possible_poses[:,:,0,0:2]
+    poses = np.reshape(poses, (poses.shape[0]*poses.shape[1], 2))
+    ax.scatter(poses[:,0], poses[:,1], s=0.1, color='black')
+
+    grid_element_size = (extent[1] - extent[0]) / heatmap_values.shape[1]
+    arrow_width = grid_element_size / 20
+
+    # Highlight best score with red circle
+    radius = np.linalg.norm(grid_element_size / 2 * np.ones(2))
+    best_pose_idx = np.unravel_index(np.argmax(pose_scores), pose_scores.shape)
+    best_pose = possible_poses[best_pose_idx]
+    best_pose_position = (best_pose[0], best_pose[1])
+    best_pose_circle = plt.Circle(best_pose_position, radius, color='red', fill=False)
+    ax.add_artist(best_pose_circle)
+
+    # Visualize orientations with highest score at each position
     for x in range(possible_poses.shape[0]):
         for y in range(possible_poses.shape[1]):
+            # Don't plot arrow if score is zero
+            if np.allclose(heatmap_values.T[x, y], 0.0):
+                continue
+
             pose_idx = highest_score_pose_idx[x, y]
             pose = possible_poses[x, y, pose_idx, :]
-            position = pose[0:3]
-            orientation = pose[3:7]
-            yaw, _, _ = tf.euler.quat2euler(orientation, axes='szyx')
+            position, orientation = split_pose_array(pose)
+            _, _, yaw = tf.euler.quat2euler(orientation, axes='sxyz')
 
             highest_arrow_length = 0.001
-            dx = np.cos(yaw) * highest_arrow_length
-            dy = np.sin(yaw) * highest_arrow_length
-            plt.arrow(position[0], position[1], dx, dy, width = 0.1, color='black')
-            #print(f'({position[0]},{position[1]})')
+            # Yaw needs to be offset by +90 degrees to be the correct direction in the visualization
+            dx = np.cos(yaw + np.deg2rad(90)) * highest_arrow_length
+            dy = np.sin(yaw + np.deg2rad(90)) * highest_arrow_length
+            ax.arrow(position[0], position[1], dx, dy, width = arrow_width, color='black')
 
 
     for sign_type in sign_types:
@@ -96,39 +199,32 @@ def show_heatmap(possible_poses, pose_scores, landmark_list):
 
         # Visualize landmark positions
         color = util.color_tuple_bgr_to_plt(detection.sign_type_colors[sign_type])
-        plt.scatter(pos_x, pos_y, s=100, color=color)
+        ax.scatter(pos_x, pos_y, s=100, color=color)
 
         # Visualize landmark directions
         for landmark in landmarks_of_type:
             direction = landmark.direction
-            # TODO Remove
-            direction = np.array([-1.0, 0.0, 0.0])
 
             # Don't plot arrow if direction is all zeros
             if not direction.any():
                 continue
 
-            sign_arrow_length = 10
+            sign_arrow_length = 1
             dx = direction[0] * sign_arrow_length
             dy = direction[1] * sign_arrow_length
-            plt.arrow(landmark.x, landmark.y, dx, dy, width = 3, color=color)
-
-        print(list(landmarks_of_type))
+            ax.arrow(landmark.x, landmark.y, dx, dy, width = 2*arrow_width, color=color)
 
     plt.show()
 
 
 if __name__ == '__main__':
-    landmark_list = mapping.load_map(MAP_PATH)
+    landmark_list = util.pickle_load(MAP_PATH)
     query_image = cv2.imread(QUERY_IMAGE_PATH)
     assert(query_image is not None)
 
     camera = get_camera_malaga_extract_07_right()
 
-    # TODO Use all sign types
-    #sign_types = detection.ALL_SIGN_TYPES
-    sign_types = {detection.TrafficSignType.CROSSING}
-    sign_types = {}
+    sign_types = detection.ALL_SIGN_TYPES
 
     possible_poses, pose_scores = get_pose_scores(landmark_list, query_image, camera, sign_types)
     show_heatmap(possible_poses, pose_scores, landmark_list)
--- mapping/score.py
+++ mapping/score.py
@@ -1,101 +1,179 @@
+import matplotlib.pyplot as plt
 import pickle
 from collections import namedtuple
 from detection import TrafficSignDetection, TrafficSignType
+import detection
 import math
 import numpy as np
+from scipy.stats import multivariate_normal
+from numpy import interp
+
 
 IMAGE_WIDTH = 1024
 IMAGE_HEIGHT = 768
 ALL_SIGN_TYPES = [TrafficSignType.CROSSING, TrafficSignType.YIELD, TrafficSignType.ROUNDABOUT]
 
-# NOTE: Guassain score is between 0 and 1
 
-def calculate_guassian(sigma,x,y):
-    ret = 1/(2 * math.pi * sigma * sigma)
-    distance_sq = (x**2) + (y**2)
-    exp = -1*distance_sq/(2*sigma*sigma)
-    ret *= math.exp(exp)
-    return ret
-# for this score we convert the bounding boxes into circles
-#  keeping the same center and defining the radius as half
-# of the diaganol of the box. We create a uniform distribution in the circles
-# we then use a guassian distribution on the remaining pixels for each bounding box
-# and then we average the values for the remaining pixels.
+def gaussian_grid(width, height, mean, cov):
+    var = multivariate_normal(mean=mean, cov=cov)
 
-# Do to the circle transformation this needs to be modified in cases where the bounding box is in the corner or edges
-def create_gaussian_score_arr_single_class(detectionSet):
-    ret = np.zeros(shape=(IMAGE_WIDTH,IMAGE_HEIGHT))
-    temp = np.zeros(shape=(IMAGE_WIDTH,IMAGE_HEIGHT))
-    total = 0
+    x, y = np.meshgrid(range(width), range(height))
+    grid = np.dstack((x,y))
+
+    # var.pdf() takes a list of points, which is why we need to reshape the
+    # input and then reshape the output back into the grid shape
+    sample_coordinates = np.reshape(grid, (width*height, 2))
+    assert(np.allclose(np.dstack((x,y)), np.reshape(sample_coordinates, (height, width, 2))))
+
+    samples = np.reshape(var.pdf(sample_coordinates), (height, width)).T
+    assert(samples.shape[0] == width)
+    assert(samples.shape[1] == height)
+
+    return samples
+
+
+def create_gaussian_score_arr(detectionSet):
+    # Downsampling to increase performance
+    array_width = 20
+    scale = array_width / IMAGE_WIDTH
+    array_height = int(scale * IMAGE_HEIGHT)
+    ret = np.zeros(shape=(array_width, array_height))
+
     for detection in detectionSet:
-        total += 1
-        radius = math.sqrt((detection.height**2) + (detection.width**2))/2
-        center_x = detection.x
-        center_y = detection.y
-        uniform_pixel_value = 1/(math.pi * (radius**2))
-        for i in range(0,IMAGE_WIDTH):
-            for j in range(0,IMAGE_HEIGHT):
-                distance = math.sqrt(((i - center_x)**2) + ((j - center_y)**2))
-                if distance <= radius:
-                    ret[i][j] = uniform_pixel_value
-                else:
-                    temp[i][j] += calculate_guassian(radius,i - center_x, j - center_y)
+        mean = np.array([detection.x, detection.y]) * scale
 
-    assert(total > 0)
+        deviation_x = detection.width
+        deviation_y = detection.height
+        deviation = np.array([deviation_x, deviation_y]) * scale
+        cov = np.diag(10*(deviation**2))
 
-    for i in range(0,IMAGE_WIDTH):
-        for j in range(0,IMAGE_HEIGHT):
-            if(ret[i][j] == 0):
-                ret[i][j] = temp[i][j]/total
+        distribution = gaussian_grid(array_width, array_height, mean, cov)
+        ret += distribution
+
     return ret
 
-def calculate_guassian_score_single_class(detectionSet1,detectionSet2):
-    ref = create_gaussian_score_arr_single_class(detectionSet1)
-    score = 0
-    num_detections = 0
-    for detection in detectionSet2:
-        radius = math.sqrt((detection.height**2) + (detection.width**2))/2
-        center_x = detection.x
-        center_y = detection.y
-        num_detections += 1
-        for i in range(int(center_x - radius), int(center_x + radius)):
-            if (i >= IMAGE_WIDTH) or (i < 0):
-                continue
-            for j in range(int(center_y - radius), int(center_y + radius)):
-                if(j >= IMAGE_HEIGHT) or (j < 0):
-                    continue
-                distance = math.sqrt(((center_x - i) ** 2) + ((center_y - j) ** 2))
-                if(distance <= radius):
-                    score += ref[i][j]
-    if(num_detections == 0):
-        return 0     
-    return score/num_detections
 
-# dectectionSet1 = P
-# dectectionSet2 = Q
-def get_score(detectionSet1, detectionSet2, sign_types):
+def calculate_guassian_match_score(detections_predicted, detections_query, debug=False):
+    gaussian_query = create_gaussian_score_arr(detections_query)
+    gaussian_predicted = create_gaussian_score_arr(detections_predicted)
+
+    correlation = gaussian_query * gaussian_predicted
+
+    # Source for upper bound: https://math.stackexchange.com/a/22579
+    max_total_correlation = np.sqrt(np.sum(gaussian_query**2) * np.sum(gaussian_predicted**2))
+    total_correlation = np.sum(correlation)
+    if max_total_correlation == 0.0:
+        total_correlation_normalized = 0.0
+    else:
+        total_correlation_normalized = np.sum(correlation) / max_total_correlation
+
+    diff = np.abs(gaussian_query - gaussian_predicted)
+
+    max_total_diff = np.sum(gaussian_query) + np.sum(gaussian_predicted)
+    total_diff = np.sum(diff)
+    if max_total_diff == 0.0:
+        total_diff_normalized = 0.0
+    else:
+        total_diff_normalized = total_diff / max_total_diff
+
+    assert(0 <= total_correlation_normalized <= 1)
+    assert(0 <= total_diff_normalized <= 1)
+    match_score = total_correlation_normalized - total_diff_normalized
+    match_score = interp(match_score, [-1,1], [0,1])
+
+    if debug:
+        print('-----------')
+        print(f'total_correlation={total_correlation}')
+        print(f'max_total_correlation={max_total_correlation}')
+        print(f'total_diff={total_diff}')
+        print(f'max_total_diff={max_total_diff}')
+        print(f'match_score={match_score}')
+#         if len(detections_query) > 0:
+        show_distribution(gaussian_query)
+        show_distribution(gaussian_predicted)
+#         show_distribution(diff)
+
+    return match_score
+
+
+def show_distribution(distribution):
+    #print(f'sum={np.sum(distribution)}')
+    plt.imshow(distribution.T)
+    plt.colorbar()
+    plt.show()
+
+
+def get_score(detections_predicted, detections_query, sign_types, debug=False):
     guassian_score = 0
     for sign_type in sign_types:
-        set1 = list(filter(lambda d: d.sign_type == sign_type, detectionSet1))
-        set2 = list(filter(lambda d: d.sign_type == sign_type, detectionSet2))
-        guassian_score += calculate_guassian_score_single_class(set1,set2)
+        if debug:
+            print(f'Calculating score for sign type \'{sign_type}\'')
+        set1 = list(filter(lambda d: d.sign_type == sign_type, detections_predicted))
+        set2 = list(filter(lambda d: d.sign_type == sign_type, detections_query))
 
-    return guassian_score
+        guassian_score += calculate_guassian_match_score(set1, set2, debug)
+
+    average_gaussian_score = guassian_score / len(sign_types)
+
+    return average_gaussian_score
 
 
 if __name__ == '__main__':
-    testImg = "img_CAMERA1_1261229988.730080_right.jpg"
-    with open('dectections.pkl', 'rb') as dectections:
-    
-        # Step 3
-        detectionsDict = pickle.load(dectections)
-        testImgDections = detectionsDict[testImg]
-        for img in detectionsDict:
-            detectionList = detectionsDict[img]
-            if(len(detectionList) == 0):
-                continue
-            total_score, guassian_score = get_score(testImgDections,detectionList,ALL_SIGN_TYPES)
-            print("{} --- {}\n original score{}\n guassian score{}".format(testImg,img,total_score,guassian_score))
+    ALL_SIGN_TYPES = [TrafficSignType.CROSSING, TrafficSignType.YIELD, TrafficSignType.ROUNDABOUT]
+    ALL_SIGN_TYPES = [TrafficSignType.CROSSING]
 
+    detectionPredicted1 = detection.TrafficSignDetection(x=438, y=223, width=40, height=40, sign_type=detection.TrafficSignType.CROSSING, score=0)
+    detectionPredicted2 = detection.TrafficSignDetection(x=800, y=500, width=40, height=40, sign_type=detection.TrafficSignType.CROSSING, score=0)
+    detectionPredicted3 = detection.TrafficSignDetection(x=0, y=0, width=40, height=40, sign_type=detection.TrafficSignType.CROSSING, score=0)
+    detectionPredicted4 = detection.TrafficSignDetection(x=0, y=0, width=80, height=80, sign_type=detection.TrafficSignType.CROSSING, score=0)
+    detectionPredicted5 = detection.TrafficSignDetection(x=438, y=223, width=80, height=80, sign_type=detection.TrafficSignType.CROSSING, score=0)
 
-    
+    detectionQuery1 = detection.TrafficSignDetection(x=438, y=223, width=40, height=40, sign_type=detection.TrafficSignType.CROSSING, score=0)
+    detectionQuery2 = detection.TrafficSignDetection(x=500, y=300, width=40, height=40, sign_type=detection.TrafficSignType.CROSSING, score=0)
+
+    # Should go from highest to lowest score
+    predicted = [detectionPredicted1]
+    query = [detectionQuery1]
+    guassian_score = get_score(predicted, query, ALL_SIGN_TYPES, debug=True)
+    print(f'guassian_score={guassian_score}')
+    # >
+    predicted = []
+    query = []
+    guassian_score = get_score(predicted, query, ALL_SIGN_TYPES, debug=True)
+    print(f'guassian_score={guassian_score}')
+    # > (?)
+    predicted = [detectionPredicted5]
+    query = [detectionQuery1]
+    guassian_score = get_score(predicted, query, ALL_SIGN_TYPES, debug=True)
+    print(f'guassian_score={guassian_score}')
+    # >
+    predicted = [detectionPredicted1, detectionPredicted2, detectionPredicted3]
+    query = [detectionQuery1]
+    guassian_score = get_score(predicted, query, ALL_SIGN_TYPES, debug=True)
+    print(f'guassian_score={guassian_score}')
+    # > (?)
+    predicted = [detectionPredicted1]
+    query = [detectionQuery1, detectionQuery2]
+    guassian_score = get_score(predicted, query, ALL_SIGN_TYPES, debug=True)
+    print(f'guassian_score={guassian_score}')
+    # > (?)
+    predicted = [detectionPredicted2]
+    query = [detectionQuery1]
+    guassian_score = get_score(predicted, query, ALL_SIGN_TYPES, debug=True)
+    print(f'guassian_score={guassian_score}')
+    # >
+    predicted = []
+    query = [detectionQuery1]
+    guassian_score = get_score(predicted, query, ALL_SIGN_TYPES, debug=True)
+    print(f'guassian_score={guassian_score}')
+    # ==
+    print('equal')
+    predicted = [detectionPredicted1]
+    query = []
+    guassian_score = get_score(predicted, query, ALL_SIGN_TYPES, debug=True)
+    print(f'guassian_score={guassian_score}')
+    # == (?)
+    predicted = [detectionPredicted5]
+    query = []
+    guassian_score = get_score(predicted, query, ALL_SIGN_TYPES, debug=True)
+    print(f'guassian_score={guassian_score}')
