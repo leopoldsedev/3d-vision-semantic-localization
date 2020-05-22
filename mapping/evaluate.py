dba9d23b0e4e7bdb6e704bf97d348d38dfaeedd2
patricia2602 <waa300386@gmail.com>
2020-05-22 02:00:57 +0200

    this should generate a rank matrix...waiting for full converted gps/imu data

--- mapping/evaluate.py
+++ mapping/evaluate.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import mean_squared_error as mse
+import re
 import numpy as np
 import random as rand
 import localization
@@ -16,19 +17,19 @@ np.set_printoptions(threshold=sys.maxsize)
 # detections
 # timestamps = [1261230001.030210]
 # timestamps = [1261230001.080210]
-timestamps = [1261230001.130214]
-timestamps = [1261230001.180217]
-timestamps = [1261230036.630518]
-timestamps = [1261230001.430199]
+# timestamps = [1261230001.130214]
+# timestamps = [1261230001.180217]
+# timestamps = [1261230036.630518]
+# timestamps = [1261230001.430199]
+
 DETECTIONS_PATH = "/home/patricia/3D/detections/detections_07_right.pickle"
-SCORES_PATH = "/home/patricia/3D/queryscores/07_right_map/img_CAMERA1_%s_right.jpg.pickle"%(str(timestamps[0]))
-POSES_PATH = "/home/patricia/Downloads/map_07_possible_poses.pickle"
-gps_full_data, imu_full_data = ground_truth_estimator.load_gps_and_imu_data('./data/07/gps.csv', './data/07/imu.csv')
-estimator = GroundTruthEstimator(gps_full_data, imu_full_data, print_kf_progress=True)
-positions = estimator.get_position(timestamps, method='cubic')
 
 def get_rank(timestamps):
-    
+    SCORES_PATH = "/home/patricia/3D/queryscores/07_right_map/img_CAMERA1_%s_right.jpg.pickle"%(str(timestamps[0]))
+    POSES_PATH = "/home/patricia/Downloads/map_07_possible_poses.pickle"
+    gps_full_data, imu_full_data = ground_truth_estimator.load_gps_and_imu_data('./data/07/gps.csv', './data/07/imu.csv')
+    estimator = GroundTruthEstimator(gps_full_data, imu_full_data, print_kf_progress=True)
+    positions = estimator.get_position(timestamps, method='cubic')
     gps = positions[0][0:2]
     with open(DETECTIONS_PATH, 'rb') as file:
         detections = pickle.load(file)
@@ -36,7 +37,7 @@ def get_rank(timestamps):
         pickle_scores = pickle.load(file)
     with open('/home/patricia/Downloads/map_07_possible_poses.pickle', 'rb') as file:
         pickle_poses = pickle.load(file)
-    num_detections = np.asarray(detections.get('img_CAMERA1_1261230001.030210_right.jpg')).shape[0]
+    num_detections = np.asarray(detections.get('img_CAMERA1_%s_right.jpg'%(str(timestamps[0])))).shape[0]
     print("detected signs:")
     print(num_detections)
     num_scores = 1
@@ -89,6 +90,22 @@ def get_rank(timestamps):
     print(correctness)
     return correctness
 
+def iterate_queries():
+    with open(DETECTIONS_PATH, 'rb') as file:
+        detections = pickle.load(file)
+    rank = []
+    for key in detections.keys():
+        pattern = re.compile('img_CAMERA1_(\d*.\d*)_(right|left).jpg')
+        match = pattern.match(key)
+        timestamp = match.group(1)
+        print(timestamp)
+        rank.append(get_rank(timestamp))
+    return rank
+
 if __name__ == '__main__':
-    timestamps = [1261230001.430199]
-    rank = get_rank(timestamps)
+    # timestamps = [1261230001.430199]
+    # timestamps = [1261230001.530205]
+    # rank = get_rank(timestamps)
+    rank = iterate_queries()
+for keys in detections.keys():
+    print(rank)
