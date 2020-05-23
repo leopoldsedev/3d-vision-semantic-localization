c9420c9d174c97e035ba0d2a0f60fe2feb2326b1
patricia2602 <waa300386@gmail.com>
2020-05-23 13:39:26 +0200

    tried with different sets

--- mapping/evaluate.py
+++ mapping/evaluate.py
@@ -5,6 +5,7 @@ import random as rand
 import localization
 from triangulation import MapLandmark, ImagePose, get_camera_malaga_extract_07_right, ColmapCamera, malaga_car_pose_to_camera_pose
 import detection
+from detection import TrafficSignType
 import util
 import cv2
 import sys
@@ -12,15 +13,19 @@ import pickle
 import ground_truth_estimator
 from ground_truth_estimator import GroundTruthEstimator
 import matplotlib.pyplot as plt
+from collections import namedtuple
+
 np.set_printoptions(threshold=sys.maxsize)
 
-
-DETECTIONS_PATH = "/home/patricia/3D/detections/detections_07_right.pickle"
+N = 500
+TrafficSignDetection = namedtuple('TrafficSignDetection', ['x', 'y', 'width', 'height', 'sign_type', 'score'])
+#DETECTIONS_PATH = "/home/patricia/3D/detections/detections_07_right.pickle"
+DETECTIONS_PATH = "/home/patricia/3D/detections/detections_08_right.pickle"
 
 gps_full_data = np.load("./transform_routes/transf_routes_overlap/routeFull_in_route7coords.npy")
 imu_full_data = np.genfromtxt("/home/patricia/3D/malaga-urban-dataset_IMU.txt", skip_header=1)
 def get_rank(timestamps):
-    SCORES_PATH = "/home/patricia/3D/queryscores/07_right_map/img_CAMERA1_%s_right.jpg.pickle"%(str(timestamps))
+    SCORES_PATH = "/home/patricia/3D/queryscores/08_right/img_CAMERA1_%s_right.jpg.pickle"%(str(timestamps))
     POSES_PATH = "/home/patricia/Downloads/map_07_possible_poses.pickle"
 #    gps_full_data, imu_full_data = ground_truth_estimator.load_gps_and_imu_data('./data/07/gps.csv', './data/07/imu.csv')
 #    gps_full_data = np.load("./transform_routes/transf_routes_overlap/routeFull_in_route7coords.npy")
@@ -69,25 +74,31 @@ def get_rank(timestamps):
         sorted_combo = combo[sorted_combo_idx]
         sorted_predicts = np.delete(sorted_combo, np.s_[-1:], axis=1)
         # leave only (x,y)
-        top_predicts = sorted_predicts[-100:]
+
+        top_predicts = sorted_predicts[-N:]
         # print(top_predicts)
         
         correct = 0 
         errors = []
         for predict in top_predicts:
             errors.append(mse(gps,predict))
-            # print(predict,mse(gps,predict))
-        
+        print(top_predicts.shape)
+        print(len(errors))
         # percentage of correctness in each query (by Pedro)
         precision = []
+        rank = 0
         for i in range(len(errors)):
-            if errors[-i]<5:
+            if errors[-i]<100:
+                if i == 0:
+                    rank = 0
+                    precision.append([1 for j in range(N)])
                 rank = i
-                precision.append([0 for j in range(i-1)]+[1 for j in range(100-i+1)])
+                print(rank)
+                precision.append([0 for j in range(i-1)]+[1 for j in range(N-i+1)])
                 break
         if not precision:
-            rank = 100
-            precision.append([0 for j in range(100)])
+            rank = N
+            precision.append([0 for j in range(N)])
         
         # print(precision)
         # print("precision and rank")
@@ -96,7 +107,7 @@ def get_rank(timestamps):
         correctness = np.sum(precision)
         precision = np.asarray(precision)
         print("wat we call rank...")
-        print(correctness)
+ #       print(correctness)
     return precision
 
 def iterate_queries():
@@ -105,8 +116,8 @@ def iterate_queries():
 #    rank = get_rank(timestamps)
     with open(DETECTIONS_PATH, 'rb') as file:
         detections = pickle.load(file)
-    rank = [0 for j in range(100)]
-    for key in list(detections.keys())[100:]:
+    rank = [0 for j in range(N)]
+    for key in list(detections.keys())[50:]:
         pattern = re.compile('img_CAMERA1_(\d*.\d*)_(right|left).jpg')
         match = pattern.match(key)
         timestamp = match.group(1)
@@ -124,8 +135,10 @@ if __name__ == '__main__':
 #    print(rank)
     rank = np.asarray(rank)
     sum_rank = np.sum(rank,axis=0)
-#    print(rank.shape)
+    print(rank.shape)
 #    print(sum_rank)
-#    print(sum_rank.shape)
-    plt.plot(sum_rank)
+    print(sum_rank.shape)
+    normalized_sum_rank = [100*i/rank.shape[0] for i in sum_rank]
+#    print(normalized_sum_rank)
+    plt.plot(normalized_sum_rank)
     plt.show()
